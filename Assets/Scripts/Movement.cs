97263eafece9484a55669626ba658cbde9e7307a
Yee <yee.gamedev@gmail.com>
2022-03-19 12:00:27 -0600

    Basic Test With New Input System

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    Vector2 moveInput;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+}
