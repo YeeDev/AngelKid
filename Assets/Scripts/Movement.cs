f6e2750cd1de52b8e75129365baed504a35f095e
Yee <yee.gamedev@gmail.com>
2022-03-19 19:02:19 -0600

    Player Moves

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,21 +5,33 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10;
+
     Vector2 moveInput;
+    Rigidbody2D rb2D;
+
 
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        Run();
     }
 
     private void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+        
+    }
+
+    private void Run()
+    {
+        Vector2 playerVelocity = moveInput * moveSpeed;
+        playerVelocity.y = rb2D.velocity.y;
+
+        rb2D.velocity = playerVelocity;
     }
 }
