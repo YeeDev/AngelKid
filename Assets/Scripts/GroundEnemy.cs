1547b1011c1fd9d125d19a41f2fd6d745a17e1b7
Yee <yee.gamedev@gmail.com>
2022-03-20 20:15:45 -0600

    There Are Now Enemies

---
+++ Assets/Scripts/GroundEnemy.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using AK.Movements;
+
+[RequireComponent(typeof(Mover))]
+public class GroundEnemy : MonoBehaviour
+{
+    [SerializeField] bool ignoresFalls = false;
+    [Range(-1, 1)][SerializeField] float movingDirection = 1f;
+    [SerializeField] Collider2D mainCollider = null;
+
+    Mover mover;
+
+    private void Awake()
+    {
+        mover = GetComponent<Mover>();
+
+        if (movingDirection < 0) { Flip(true); }
+    }
+
+    private void Update() { MoveBehaviour(); }
+
+    private void MoveBehaviour()
+    {
+        if (ignoresFalls && !mainCollider.IsTouchingLayers(LayerMask.GetMask("Jumpable")))
+        {
+            mover.StopRigidbody(true);
+            return;
+        }
+
+        mover.Move(movingDirection, 0, false);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) { if (collision.CompareTag("Ground") && !ignoresFalls) { Flip(); } }
+
+    private void OnTriggerEnter2D(Collider2D collision) { if (collision.CompareTag("Wall")) { Flip(); } }
+
+    private void Flip(bool ignoreDirection = false)
+    {
+        movingDirection = ignoreDirection ? movingDirection : movingDirection * -1;
+        Vector3 flippedScale = transform.localScale;
+        flippedScale.x = movingDirection;
+        transform.localScale = flippedScale;
+    }
+}
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -19,7 +19,10 @@ namespace AK.Movements
         }
 
         public void SetGravity(bool setToInital, float gravityScale = 0) { rb.gravityScale = setToInital ? initialGravity : gravityScale; }
-        public void StopRigidbody() { rb.velocity = Vector2.zero; }
+        public void StopRigidbody(bool keepFallSpeed = false)
+        {
+            rb.velocity = keepFallSpeed ? new Vector2(0, rb.velocity.y) : Vector2.zero;
+        }
 
         public void Move(float xAxis, float yAxis, bool isClimbing) { rb.velocity = CalculateDirectionalSpeed(xAxis, yAxis, isClimbing); }
 
