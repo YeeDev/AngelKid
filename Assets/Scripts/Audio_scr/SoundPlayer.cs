030b3b80960956785f08caea5499b46e6a5aea8d
Yee <yee.gamedev@gmail.com>
2022-04-06 17:59:49 -0500

    Jump Sound Variety Implemented

--- Assets/Scripts/Audio_scr/SoundPlayer.cs
+++ Assets/Scripts/Audio_scr/SoundPlayer.cs
@@ -5,6 +5,10 @@ public class SoundPlayer : MonoBehaviour
     [SerializeField] AudioSource jumpSource = null;
     [SerializeField] AudioSource shotSource = null;
 
-    public void PlayJumpClip() { jumpSource.Play(); }
+    public void PlayJumpClip()
+    {
+        jumpSource.pitch = Random.Range(1.05f, 1.15f);
+        jumpSource.Play();
+    }
     public void PlayShootClip() { shotSource.Play(); }
 }
